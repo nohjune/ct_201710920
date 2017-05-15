1cb018d1610f28ce441060edcb749581cec73c0e
nohjune <no0556@naver.com>
2017-05-15 15:13:11 +0900

    Add files via upload

---
+++ ct_2017/baramgaebi2.py
@@ -0,0 +1,19 @@
+﻿import turtle 
+wn=turtle.Screen() 
+t1=turtle.Turtle()
+turnby=45
+size=100
+oldpos=t1.pos()
+oldhead=t1.heading()
+num=1
+def wind(size):
+    t1.fd(size)
+    t1.rt(90)
+    t1.fd(size)
+    t1.penup()
+    t1.setpos(oldpos)
+    t1.setheading(oldhead+turnby*num)
+    t1.pendown()
+for num in range(1,9):
+    wind(size)
+    num+=1
